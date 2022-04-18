cabc6cd9e33259d0ea9b38611e42f5205830188c
JadElHakim <jh.jadhakim@gmail.com>
2022-04-18 13:45:15 +0300

    signup screen edits

--- app/src/main/java/com/cyberspacesolutions/cyberspace/SignupScreen.java
+++ app/src/main/java/com/cyberspacesolutions/cyberspace/SignupScreen.java
@@ -2,7 +2,9 @@ package com.cyberspacesolutions.cyberspace;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SignupScreen extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class SignupScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup_screen);
+    }
+    public void onBackClick(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
