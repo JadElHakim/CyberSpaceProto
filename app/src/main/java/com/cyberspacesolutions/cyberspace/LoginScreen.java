e84e65411089aa3146535d1e22b349a4a9f2758e
JadElHakim <jh.jadhakim@gmail.com>
2022-04-18 13:31:43 +0300

    loginScreen/signUpScreen

--- app/src/main/java/com/cyberspacesolutions/cyberspace/LoginScreen.java
+++ app/src/main/java/com/cyberspacesolutions/cyberspace/LoginScreen.java
@@ -6,16 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_screen);
     }
 
-    public void onLoginClick(View v){
-        Intent intent = new Intent(this, LoginScreen.class);
+    public void onBackClick(View view){
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
