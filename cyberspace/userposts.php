c67d42269f631ff1733cacbded81b24deb3c454f
JadElHakim <jh.jadhakim@gmail.com>
2022-05-05 18:24:19 +0300

    php scripts:

---
+++ cyberspace/userposts.php
@@ -0,0 +1,17 @@
+<?php
+//include database info+connection
+include("DataBaseConfig.php");
+$link= mysqli_connect($db_host,$db_user,$db_pass,$db_name);
+$username = $_POST['username'];
+$query= "select * from posts where username = '$username'";
+$result = mysqli_query($link,$query);
+if(@$result){
+ while($row = mysqli_fetch_array($result)){
+//while(mysqli_num_rows($result) != 0){
+  $arr[]= $row;
+    }
+}
+$output =json_encode($arr);
+echo($output);
+$mysqli -> close();
+?>
